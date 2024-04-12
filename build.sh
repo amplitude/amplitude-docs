286a4ba09c6d9af23b7cb1a0e5a1ec898cc8e52c
markzegarelli <mark.zegarelli@amplitude.com>
2024-04-12 10:41:07 -0700

    change package manager

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
  
 # Install PHP & WGET
-yum install -y amazon-linux-extras
+dnf install -y amazon-linux-extras
 amazon-linux-extras enable php8.1
-yum clean metadata
-yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
-yum install wget
+dnf clean metadata
+dnf install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
+dnf install wget
  
 # INSTALL COMPOSER
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
