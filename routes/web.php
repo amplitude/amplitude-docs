b5f302188209b4c2be5a5d11608227432e24a088
markzegarelli <mark.zegarelli@amplitude.com>
2024-07-12 15:26:14 -0700

    test moving content

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,13 @@ Route::statamic('json', 'json', [
     'layout' => 'blank'
    
 ]);
+
+Route::permanentRedirect('/docs/cdp/audiences', '/docs/data/audiences');
+Route::permanentRedirect('/docs/cdp/audiences/predictions', '/docs/data/audiences/predictions');
+Route::permanentRedirect('/docs/cdp/audiences/computations', '/docs/data/audiences/computations');
+Route::permanentRedirect('/docs/cdp/audiences/predictions-build', '/docs/data/audiences/predictions-build');
+Route::permanentRedirect('/docs/cdp/audiences/predictions-use', '/docs/data/audiences/predictions-use');
+Route::permanentRedirect('/docs/cdp/audiences/recommendations', '/docs/data/audiences/recommendations');
+Route::permanentRedirect('/docs/cdp/audiences/recommendations-build', '/docs/data/audiences/recommendations-build');
+Route::permanentRedirect('/docs/cdp/audiences/recommendations-use', '/docs/data/audiences/recommendations-use');
+Route::permanentRedirect('/docs/cdp/audiences/third-party-syncs', '/docs/data/audiences/third-party-syncs');
