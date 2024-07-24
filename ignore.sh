c64ab2ddcb45fc8bd0cba57ded507278656e5fe0
markzegarelli <mark.zegarelli@amplitude.com>
2024-07-24 11:02:00 -0700

    Add script to ignore Vercel builds for most users

---
+++ ignore.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# List of users
+users=("test" "SpencerFleury")
+
+# Get the contributor of the last commit
+last_contributor=$(git log -1 --pretty=format:'%an')
+
+# Check if the last contributor is in the list of users
+for user in "${users[@]}"
+do
+  if [ "$user" == "$last_contributor" ]; then
+    echo "Contributor $last_contributor is in the list."
+    exit 0
+  fi
+done
+
+echo "Contributor $last_contributor is not in the list."
+exit 1
+
+git log -1 --pretty=oneline --abbrev-commit | grep -w "\[skip deploy\]" && exit 0 || exit 1
