5a2835eae53b641d46cd7a59bcd6d557b92e4a3a
markzegarelli <mark.zegarelli@amplitude.com>
2024-07-24 11:12:53 -0700

    add [skip deploy] logic

--- ignore.sh
+++ ignore.sh
@@ -5,17 +5,20 @@ users=("markzegarelli" "SpencerFleury")
 
 # Get the contributor of the last commit
 last_contributor=$(git log -1 --pretty=format:'%an')
+last_commit_message=$(git log -1 --pretty=format:'%s')
 
 # Check if the last contributor is in the list of users
 for user in "${users[@]}"
 do
   if [ "$user" == "$last_contributor" ]; then
     echo "Contributor $last_contributor is in the list."
+        if [[ "$last_commit_message" == *"[skip deploy]"* ]]; then
+            echo "Commit message contains '[skip deploy]'."
+            exit 0
+        fi
     exit 1
   fi
 done
 
 echo "Contributor $last_contributor is not in the list."
 exit 0
-
-git log -1 --pretty=oneline --abbrev-commit | grep -w "\[skip deploy\]" && exit 0 || exit 1
