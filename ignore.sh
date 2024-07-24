85a3f36d5ea4e194867d6ca08022b43097f6ffe8
markzegarelli <mark.zegarelli@amplitude.com>
2024-07-24 11:04:42 -0700

    Update exit codes

--- ignore.sh
+++ ignore.sh
@@ -11,11 +11,11 @@ for user in "${users[@]}"
 do
   if [ "$user" == "$last_contributor" ]; then
     echo "Contributor $last_contributor is in the list."
-    exit 0
+    exit 1
   fi
 done
 
 echo "Contributor $last_contributor is not in the list."
-exit 1
+exit 0
 
 git log -1 --pretty=oneline --abbrev-commit | grep -w "\[skip deploy\]" && exit 0 || exit 1
